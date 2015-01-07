5975b78957a6ea16b465e2cd5aff87a61e1f95ae
Dion Priyandiono <ifdion@gmail.com>
2015-01-07 10:35:44 +0700

    Add sample JS plugin and sass configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,25 +5,27 @@ module.exports = function(grunt) {
   // Show elapsed time
   require('time-grunt')(grunt);
 
-  var pluginList = [
-    'bower_components/bootstrap/js/transition.js',
-    'bower_components/bootstrap/js/alert.js',
-    'bower_components/bootstrap/js/button.js',
-    'bower_components/bootstrap/js/carousel.js',
-    'bower_components/bootstrap/js/collapse.js',
-    'bower_components/bootstrap/js/dropdown.js',
-    'bower_components/bootstrap/js/modal.js',
-    'bower_components/bootstrap/js/tooltip.js',
-    'bower_components/bootstrap/js/popover.js',
-    'bower_components/bootstrap/js/scrollspy.js',
-    'bower_components/bootstrap/js/tab.js',
-    'bower_components/bootstrap/js/affix.js',
-  ];
-
   var vendorList = [
     'bower_components/jquery/dist/jquery.js',
     'bower_components/underscore/underscore.js',
-    'bower_components/backbone/backbone.js',
+  ];
+
+  var pluginList = [
+    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/transition.js',
+    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/alert.js',
+    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/button.js',
+    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/carousel.js',
+    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/collapse.js',
+    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/dropdown.js',
+    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/modal.js',
+    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tooltip.js',
+    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/popover.js',
+    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/scrollspy.js',
+    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tab.js',
+    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/affix.js',
+    'bower_components/magicsuggest/magicsuggest.js',
+    'bower_components/velocity/velocity.js',
+    'bower_components/chartjs/Chart.js',
   ];
 
   grunt.initConfig({
@@ -37,28 +39,49 @@ module.exports = function(grunt) {
         '!**/*.min.*'
       ]
     },
-    less: {
-      dev: {
-        files: {
-          'css/main.css': [
-            'less/main.less'
-          ]
-        },
+    // the sass block
+    sass: {
+      dist: {
         options: {
-          compress: false,
-        }
-      },
-      build: {
-        files: {
-          'css/main.min.css': [
-            'less/main.less'
-          ]
+          style: 'expanded'
         },
-        options: {
-          compress: true
+        files: {
+          'css/main.css': 'sass/main.scss'
         }
       }
     },
+    cssmin: {
+        dist: {
+            files: {
+                'css/main.min.css': [
+                    'css/main.css'
+                ]
+            }
+        }
+    },
+    // the less block
+    // less: {
+    //   dev: {
+    //     files: {
+    //       'css/main.css': [
+    //         'less/main.less'
+    //       ]
+    //     },
+    //     options: {
+    //       compress: false,
+    //     }
+    //   },
+    //   build: {
+    //     files: {
+    //       'css/main.min.css': [
+    //         'less/main.less'
+    //       ]
+    //     },
+    //     options: {
+    //       compress: true
+    //     }
+    //   }
+    // },
     concat: {
       options: {
         separator: ';',
@@ -73,21 +96,21 @@ module.exports = function(grunt) {
       },
     },
     uglify: {
-      plugins: {
-        files: {
-          'js/plugins.min.js': [pluginList]
-        }
-      },
       vendors: {
         files: {
           'js/vendors.min.js': [vendorList]
+        }
+      },
+      plugins: {
+        files: {
+          'js/plugins.min.js': [pluginList]
         }
       },
       main: {
         files: {
           'js/main.min.js': ['js/main.js']
         }
-      }
+      },
     },
     modernizr: {
       build: {
@@ -104,12 +127,18 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      less: {
-        files: [
-          'less/*.less',
-          'less/**/*.less'
-        ],
-        tasks: ['less:dev']
+      // do it with less
+      // less: {
+      //   files: [
+      //     'less/*.less',
+      //     'less/**/*.less'
+      //   ],
+      //   tasks: ['less:dev']
+      // },
+      // do it with sass
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
       },
       js: {
         files: [
@@ -140,12 +169,16 @@ module.exports = function(grunt) {
   ]);
   grunt.registerTask('dev', [
     'jshint',
-    'less:dev',
+    // 'less:dev',
+    'sass',
+    'cssmin',
     'concat'
   ]);
   grunt.registerTask('build', [
     'jshint',
-    'less:build',
+    // 'less:build',
+    'sass',
+    'cssmin',
     'uglify',
     'modernizr',
   ]);
